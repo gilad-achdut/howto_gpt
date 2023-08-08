a137ec7174fa4184c2a2544a5591bbe4ea762789
גלעד סקייט - אחדות ישראל <gilads@achdut.co.il>
2023-08-08 09:00:10 +0300

    initial

---
+++ howto.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import openai
+import sys
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# get openai key from environment variable
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+openai.api_key = OPENAI_API_KEY
+
+system = sys.argv[1]
+arg = ' '.join(sys.argv[2:])
+
+print(f"arg: {arg}")
+
+r = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  #model="gpt-4",
+  messages=[
+        {"role": "system", "content": "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible."},
+        {"role": "user", "content": f"Answer with only the actual command without any intro or explanation. What is the {system} command line command to {arg}"}
+    ]
+)
+
+text = r["choices"][0]["message"]["content"]
+if text.startswith('`') and text.endswith('`'):
+  text = text[1:-1]
+
+print(text)
